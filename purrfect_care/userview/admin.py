9178418693de1ee920e67a9948dc61010332dff1
Hanna Pieniążek <79666838+hannapieniazek@users.noreply.github.com>
2023-10-04 13:06:41 +0200

    Fix models names and options

--- purrfect_care/userview/admin.py
+++ purrfect_care/userview/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Species)
+admin.site.register(Breed)
+admin.site.register(Owner)
+admin.site.register(Illness)
+admin.site.register(Clinic)
+admin.site.register(Patient)
+admin.site.register(IllnessHistory)
+admin.site.register(Medication)
+admin.site.register(VisitType)
+admin.site.register(VisitSubtype)
+admin.site.register(Employee)
+admin.site.register(Visit)
+admin.site.register(Photo)
+admin.site.register(Prescription)
+admin.site.register(PrescribedMedication)
--- purrfect_care/userview/models.py
+++ purrfect_care/userview/models.py
@@ -1,136 +1,143 @@
 from django.db import models
+from django.utils import timezone
 
 # Define the 'gatunki' model
-class Gatunek(models.Model):
-    nazwa_gatunku = models.CharField(max_length=50, unique=True)
+class Species(models.Model):
+    species_name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
-        return self.nazwa_gatunku
+        return self.species_name
 
 # Define the 'rasy' model
-class Rasa(models.Model):
-    nazwa_rasy = models.CharField(max_length=50, unique=True)
-    rasy_id_gatunku = models.ForeignKey(Gatunek, on_delete=models.CASCADE)
+class Breed(models.Model):
+    breed_name = models.CharField(max_length=50, unique=True)
+    breeds_species_id = models.ForeignKey(Species, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.nazwa_rasy
+        return self.breed_name
 
 # Define the 'opiekunowie' model
-class Opiekun(models.Model):
-    imie_opiekuna = models.CharField(max_length=50)
-    nazwisko_opiekuna = models.CharField(max_length=50)
-    adres_opiekuna = models.CharField(max_length=255)
-    kod_pocztowy_opiekuna = models.CharField(max_length=20)
-    miasto_opiekuna = models.CharField(max_length=50)
-    telefon_opiekuna = models.CharField(max_length=20, unique=True)
-    email_opiekuna = models.EmailField(unique=True)
+class Owner(models.Model):
+    owner_first_name = models.CharField(max_length=50)
+    owner_last_name = models.CharField(max_length=50)
+    owner_address = models.CharField(max_length=255)
+    owner_postcode = models.CharField(max_length=20)
+    owner_city = models.CharField(max_length=50)
+    owner_phone_number = models.CharField(max_length=20, unique=True)
+    owner_email = models.EmailField(unique=True)
 
     def __str__(self):
-        return f"{self.imie_opiekuna} {self.nazwisko_opiekuna}"
+        return f"{self.owner_first_name} {self.owner_last_name}"
 
 # Define the 'choroby' model
-class Choroba(models.Model):
-    nazwa_choroby = models.CharField(max_length=255, unique=True)
+class Illness(models.Model):
+    illness_name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
-        return self.nazwa_choroby
+        return self.illness_name
 
 # Define the 'gabinety' model
-class Gabinet(models.Model):
-    nazwa_gabinetu = models.CharField(max_length=150, unique=True)
-    adres_gabinetu = models.CharField(max_length=255)
-    kod_pocztowy_gabinetu = models.CharField(max_length=20)
-    miasto_gabinetu = models.CharField(max_length=50)
-    telefon_gabinetu = models.CharField(max_length=20, unique=True)
-    email_gabinetu = models.EmailField(unique=True)
+class Clinic(models.Model):
+    clinic_name = models.CharField(max_length=150, unique=True)
+    clinic_address = models.CharField(max_length=255, unique=True)
+    clinic_postcode = models.CharField(max_length=20)
+    clinic_city = models.CharField(max_length=50)
+    clinic_phone_number = models.CharField(max_length=20, unique=True)
+    clinic_email = models.EmailField(unique=True)
 
     def __str__(self):
-        return self.nazwa_gabinetu
+        return self.clinic_name
 
 # Define the 'pacjenci' model
-class Pacjent(models.Model):
-    imie_pacjenta = models.CharField(max_length=255)
-    PLEC_CHOICES = [('male', 'Male'), ('female', 'Female')]
-    plec_pacjenta = models.CharField(max_length=10, choices=PLEC_CHOICES)
-    data_urodzenia_pacjenta = models.DateField()
-    pacjenci_id_opiekuna = models.ForeignKey(Opiekun, on_delete=models.CASCADE)
-    pacjenci_id_gatunku = models.ForeignKey(Gatunek, on_delete=models.CASCADE)
-    pacjenci_id_rasy = models.ForeignKey(Rasa, on_delete=models.CASCADE)
+class Patient(models.Model):
+    patient_name = models.CharField(max_length=255)
+    GENDER_CHOICES = [('male', 'Male'), ('female', 'Female')]
+    patient_gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
+    patient_date_of_birth = models.DateField(null=True, blank=True)
+    patients_owner_id = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    patients_species_id = models.ForeignKey(Species, on_delete=models.CASCADE)
+    patients_breed_id = models.ForeignKey(Breed, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.imie_pacjenta
+        return self.patient_name
 
 # Define the 'historie_chorob' model
-class HistoriaChoroby(models.Model):
-    historie_chorob_id_pacjenta = models.ForeignKey(Pacjent, on_delete=models.CASCADE)
-    historie_chorob_id_choroby = models.ForeignKey(Choroba, on_delete=models.CASCADE)
-    data_rozpoczecia_choroby = models.DateField()
+class IllnessHistory(models.Model):
+    illness_history_patient_id = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    illness_history_illness_id = models.ForeignKey(Illness, on_delete=models.CASCADE)
+    illness_onset_date = models.DateField()
 
 # Define the 'leki' model
-class Lek(models.Model):
-    nazwa_leku = models.CharField(max_length=255, unique=True)
+class Medication(models.Model):
+    medication_name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
-        return self.nazwa_leku
+        return self.medication_name
 
 # Define the 'typy_wizyty' model
-class TypWizyty(models.Model):
-    nazwa_typu_wizyty = models.CharField(max_length=100, unique=True)
+class VisitType(models.Model):
+    visit_type_name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return self.nazwa_typu_wizyty
+        return self.visit_type_name
 
 # Define the 'podtypy_wizyty' model
-class PodtypWizyty(models.Model):
-    nazwa_podtypu_wizyty = models.CharField(max_length=100, unique=True)
-    podtypy_wizyty_id_typu_wizyty = models.ForeignKey(TypWizyty, on_delete=models.CASCADE)
+class VisitSubtype(models.Model):
+    visit_subtype_name = models.CharField(max_length=100, unique=True)
+    visit_subtypes_visit_type_id = models.ForeignKey(VisitType, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.nazwa_podtypu_wizyty
+        return self.visit_subtype_name
 
 # Define the 'pracownicy' model
-class Pracownik(models.Model):
-    rola_pracownika = models.CharField(max_length=100)
-    imie_pracownika = models.CharField(max_length=50)
-    nazwisko_pracownika = models.CharField(max_length=50)
-    adres_pracownika = models.CharField(max_length=255)
-    kod_pocztowy_pracownika = models.CharField(max_length=20)
-    miasto_pracownika = models.CharField(max_length=50)
-    telefon_pracownika = models.CharField(max_length=20, unique=True)
-    email_pracownika = models.EmailField(unique=True)
-    haslo_pracownika = models.CharField(max_length=64, unique=True)  # Store hashed passwords here
-    pracownicy_id_gabinetu = models.ForeignKey(Gabinet, on_delete=models.CASCADE)
+class Employee(models.Model):
+    employee_role = models.CharField(max_length=100)
+    employee_first_name = models.CharField(max_length=50)
+    employee_last_name = models.CharField(max_length=50)
+    employee_address = models.CharField(max_length=255)
+    employee_postcode = models.CharField(max_length=20)
+    employee_city = models.CharField(max_length=50)
+    employee_phone_number = models.CharField(max_length=20, unique=True)
+    employee_email = models.EmailField(unique=True)
+    employee_password = models.CharField(max_length=64)
+    employees_clinic_id = models.ForeignKey(Clinic, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.imie_pracownika} {self.nazwisko_pracownika}"
+        return f"{self.employee_first_name} {self.employee_last_name}"
 
 # Define the 'wizyty' model
-class Wizyta(models.Model):
-    data_godzina_wizyty = models.DateTimeField()
-    czas_trwania_wizyty = models.TimeField()
-    wizyty_id_pacjenta = models.ForeignKey(Pacjent, on_delete=models.CASCADE)
-    wizyty_id_typu_wizyty = models.ForeignKey(TypWizyty, on_delete=models.CASCADE)
-    wizyty_id_podtypu_wizyty = models.ForeignKey(PodtypWizyty, on_delete=models.CASCADE)
-    wizyty_id_pracownika = models.ForeignKey(Pracownik, on_delete=models.CASCADE)
-    STATUS_CHOICES = [('zaplanowana', 'Zaplanowana'), ('odwolana', 'Odwolana'), ('zakonczona', 'Zakonczona')]
-    status_wizyty = models.CharField(max_length=20, choices=STATUS_CHOICES)
-    opis_wizyty = models.TextField()
-    masa_pacjenta = models.DecimalField(max_digits=5, decimal_places=2, null=True)
-    wzrost_pacjenta = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+class Visit(models.Model):
+    visit_datetime = models.DateTimeField()
+    visit_duration = models.TimeField()
+    visits_patient_id = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    visits_visit_type_id = models.ForeignKey(VisitType, on_delete=models.CASCADE)
+    visits_visit_subtype_id = models.ForeignKey(VisitSubtype, on_delete=models.CASCADE)
+    visits_employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    VISIT_STATUS_CHOICES = [('planned', 'Planned'), ('cancelled', 'Cancelled'), ('complete', 'Complete')]
+    visit_status = models.CharField(max_length=10, choices=VISIT_STATUS_CHOICES)
+    visit_description = models.TextField(null=True, blank=True)
+    patient_weight = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    patient_height = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+
 
 # Define the 'zdjecia' model
-class Zdjecie(models.Model):
-    zdjecia_id_wizyty = models.ForeignKey(Wizyta, on_delete=models.CASCADE)
-    opis_zdjecia = models.TextField()
+class Photo(models.Model):
+    photo_name = models.CharField(max_length=255)
+    photos_visit_id = models.ForeignKey(Visit, on_delete=models.CASCADE)
+    photo_description = models.TextField(null = True, blank=True)
+
+    def __str__(self):
+        return self.photo_name
 
 # Define the 'recepty' model
-class Recepta(models.Model):
-    recepty_id_wizyty = models.ForeignKey(Wizyta, on_delete=models.CASCADE)
-    recepty_id_pacjenta = models.ForeignKey(Pacjent, on_delete=models.CASCADE)
-    data_wydania_recepty = models.DateField()
+class Prescription(models.Model):
+    prescriptions_patient_id = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    prescription_date = models.DateField(default=timezone.now)
+
+
 
 # Define the 'leki_na_recepcie' model
-class LekNaRecepcji(models.Model):
-    leki_na_recepcie_id_recepty = models.ForeignKey(Recepta, on_delete=models.CASCADE)
-    leki_na_recepcie_id_leku = models.ForeignKey(Lek, on_delete=models.CASCADE)
-    ilosc_leku = models.PositiveIntegerField()
+class PrescribedMedication(models.Model):
+    prescribed_medications_prescription_id = models.ForeignKey(Prescription, on_delete=models.CASCADE)
+    prescribed_medications_medication_id = models.ForeignKey(Medication, on_delete=models.CASCADE)
+    medication_amount = models.PositiveIntegerField()
