03cc47e219d7592fcdc51b10e0da59c333a53144
Jan Szczuka <janogamesyt5@gmail.com>
2023-11-27 19:33:02 +0100

    added missing login mechanism after failed merge

--- purrfect_care/purrfectcareview/views.py
+++ purrfect_care/purrfectcareview/views.py
@@ -1,13 +1,15 @@
 from .models import Employee, Visit, VisitType, VisitSubtype, Patient, Owner, Prescription, IllnessHistory, Illness
-from django.contrib import messages
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
-from django.db.models import Q
-from django.http import HttpRequest
-from django.shortcuts import get_object_or_404, render, redirect
+from django.http import HttpRequest, JsonResponse
+from django.shortcuts import get_object_or_404, render
 from rest_framework import viewsets
 from .serializers import OwnerSerializer, VisitTypeSerializer, VisitSubtypeSerializer, PatientSerializer, \
     VisitSerializer, IllnessHistorySerializer, PrescriptionSerializer, EmployeeSerializer, PatientSideBarListSerializer, IllnessSerializer
+from datetime import datetime, timedelta
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+import json
+import jwt
+from argon2 import PasswordHasher
 
 
 class IllnessHistoryView(viewsets.ModelViewSet):
@@ -58,47 +60,45 @@ class PatientSideBarListViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Patient.objects.all()
     serializer_class = PatientSideBarListSerializer
 
-
-def login_view(request: HttpRequest):
-    if request.method == "POST":
-        email = request.POST.get("email")
-        password = request.POST.get("password")
-
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
         try:
-            employee = Employee.objects.get(Q(employee_email=email) & Q(employee_password=password))
-        except Employee.DoesNotExist:
-            employee = None
-
-        if employee is not None:
-            print("Pracownik istnieje")
-            request.session["employee_id"] = employee.id
-            return redirect("index")
-        else:
-            print("Pracownik nie istnieje")
-            messages.error(request, "Invalid email or password.")
-    print("nie weszlo w zadnego ifa")
-    # return render(request, 'purrfectcareview/login.html')
-    return render(request)
-
-
-def index(request: HttpRequest):
-    employee_id = request.session.get("employee_id")
-    employee = Employee.objects.get(id=1)
-
-    visits = Visit.objects.filter(visits_employee_id=employee)
-
-    context = {
-        'employee': employee,
-        'visits': visits,
-    }
-
-    return render(request, 'purrfectcareview/index.html', context)
-
-
-def logout_view(request: HttpRequest):
-    request.session.flush()
-    return render(request, 'purrfectcareview/login.html')
-
+            ph = PasswordHasher()
+            data = json.loads(request.body)
+            email = data.get('email')
+            password = data.get('password')
+            print(email)
+            print(password)
+            user = Employee.objects.get(employee_email=email)
+            if user is not None and ph.verify(user.employee_password, password):
+                expiration_time = timezone.now() + timedelta(hours=2)
+                user_data = {
+                "id": user.id,
+                "employee_role": user.employee_role,
+                "employee_first_name": user.employee_first_name,
+                "employee_last_name": user.employee_last_name,
+                "employees_clinic_id": user.employees_clinic_id.id
+                }
+                token_payload = {
+                    'user_id': user.id,
+                    'exp': expiration_time.timestamp()
+                }
+                token = jwt.encode(token_payload, 'your_secret_key', algorithm='HS256')
+                print(expiration_time.timestamp())
+                print(datetime.fromtimestamp(expiration_time.timestamp())
+)
+                # Authentication successful
+                return JsonResponse({'message': 'Login successful', 'token': token, 'expiration_time': int(expiration_time.timestamp()), 'employee': user_data}, content_type='application/json')
+            else:
+                # Authentication failed
+                return JsonResponse({'message': 'Invalid credentials'}, status=401)
+        except Exception as e:
+            # Handle other exceptions
+            print(f"Error: {e}")
+            return JsonResponse({'message': 'Internal Server Error'}, status=500)
+    else:
+        return JsonResponse({'message': 'Method not allowed'}, status=405)
 
 def patients_view(request: HttpRequest):
     patients = Patient.objects.all
